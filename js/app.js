3ff37e159680bf8bf43f984c38af094732360eca
nahuel molina <matycarp1430@gmail.com>
2023-04-02 15:49:39 -0300

    agregue barra de busqueda desplegable con sombra, agregue variables para los colores

--- js/app.js
+++ js/app.js
@@ -10,25 +10,38 @@ function animateBars(){
    
 }
 
-/* 
-document.getElementById("btn__menu").addEventListener("click", mostrar_menu);
- 
-var nav = document.getElementById("nav");
-
-
-function mostrar_menu(){
-    nav.style.left="0px";
-}
-
-
-function ocultar_menu(){
-    
-    nav.style.left="-700px"
-   
-}
- */
-
 document.getElementById("btn__menu").addEventListener("click", function(){
 
     document.getElementById("nav").classList.toggle("mostrar");
+   
 });
+
+//ejecutando funciones
+
+document.getElementById("icon-search").addEventListener("click", mostrarBuscador);
+document.getElementById("sombra-ctn-search").addEventListener("click", ocultarBuscador);
+
+//BUSCADOR DE CONTENIDO-----------------------------------------------
+
+var search=  document.getElementById("ctn-bars-search");
+var sombra=  document.getElementById("sombra-ctn-search");
+var inputSearch=  document.getElementById("inputSearch");
+var box_search=  document.getElementById("box-search");
+
+//funcion mostrar buscador
+
+function mostrarBuscador(){
+   search.style.top="135px";
+   sombra.style.display="block";
+   inputSearch.focus();
+
+}
+
+//ocultar buscador--------------
+
+function ocultarBuscador(){
+    search.style.top="0px";
+    sombra.style.display="none";
+    inputSearch.value="";
+}
+
